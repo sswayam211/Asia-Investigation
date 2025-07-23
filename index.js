bc4a493eef56eb75992d159b17b6a51fdda650a2
sswayam211 <sswayam211@gmail.com>
2025-07-23 11:15:58 +0530

    update logo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 try {
     let topNav = document.querySelector('header .top-nav');
     let logo = document.querySelector('header .logoo');
+    let logoImg = document.querySelector('header .logoo img');
     let prevScroll = 0;
 
     window.addEventListener('scroll', () => {
@@ -12,13 +13,16 @@ try {
             if (currScroll > document.querySelector('header').clientHeight) {
                 topNav.classList.remove('d-lg-flex');
                 logo.style.borderRight = 'none';
+                logoImg.style.height = '60px';
             } else {
                 topNav.classList.add('d-lg-flex');
                 logo.style.borderRight = '1px solid #dee2e6';
+                logoImg.style.height = '100px';
                 // prevScroll = currScroll;
             }
         } else {
             logo.style.borderRight = 'none';
+            logoImg.style.height = '60px';
         }
 
 
