ebc0f3070ed6b364bd0e19c0376c965f749e0645
sswayam211 <sswayam211@gmail.com>
2025-07-31 11:15:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// header js 
-try {
-    let topNav = document.querySelector('header .top-nav');
-    let logo = document.querySelector('header .logoo');
-    let logoImg = document.querySelector('header .logoo img');
-    let prevScroll = 0;
-
-    window.addEventListener('scroll', () => {
-        let currScroll = window.scrollY;
-        // console.log(currScroll);
-        if (window.innerWidth > 992) {
-            if (currScroll > document.querySelector('header').clientHeight) {
-                topNav.classList.remove('d-lg-flex');
-                logo.style.borderRight = 'none';
-                logoImg.style.height = '60px';
-            } else {
-                topNav.classList.add('d-lg-flex');
-                logo.style.borderRight = '1px solid #dee2e6';
-                logoImg.style.height = '100px';
-                // prevScroll = currScroll;
-            }
-        } else {
-            logo.style.borderRight = 'none';
-            logoImg.style.height = '60px';
-        }
-
-
-    });
-} catch (err) {
-    console.log(err);
-}
-
-
 // testimonial seaction 
 try {
     let testimonialContent = document.querySelector('.testimonial-box .content');
@@ -151,7 +118,7 @@ try {
     // });
     function startScroll() {
         clientBar.scrollLeft += 1;
-        if (clientBar.scrollLeft >= clientBar.scrollWidth - clientBar.clientWidth ) {
+        if (clientBar.scrollLeft >= clientBar.scrollWidth - clientBar.clientWidth) {
             clientBar.scrollLeft = 0;
         }
     }
